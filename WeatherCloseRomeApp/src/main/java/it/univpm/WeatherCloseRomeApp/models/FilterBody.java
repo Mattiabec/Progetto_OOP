984aa3408036b23ba3f868e4d090fd86b3cf7f2d
Alessandro Fermanelli <alessandro.fermanelli99@gmail.com>
2021-03-05 13:08:40 +0100

    filterbody.

--- WeatherCloseRomeApp/src/main/java/it/univpm/WeatherCloseRomeApp/models/FilterBody.java
+++ WeatherCloseRomeApp/src/main/java/it/univpm/WeatherCloseRomeApp/models/FilterBody.java
@@ -2,4 +2,28 @@ package it.univpm.WeatherCloseRomeApp.models;
 
 public class FilterBody {
 
+	private int count;
+	private String period;
+	private String data; //Formato data : yyyy-MM-dd
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
 }
