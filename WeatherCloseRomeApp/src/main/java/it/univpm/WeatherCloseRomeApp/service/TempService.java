b807a6b02b736d1583449a1d6198a6b22ad194b1
Mattia Beccerica <mattiabec@gmail.com>
2021-03-11 15:51:06 +0100

    Commento TempService

--- WeatherCloseRomeApp/src/main/java/it/univpm/WeatherCloseRomeApp/service/TempService.java
+++ WeatherCloseRomeApp/src/main/java/it/univpm/WeatherCloseRomeApp/service/TempService.java
@@ -6,13 +6,19 @@ import java.util.Vector;
 import it.univpm.WeatherCloseRomeApp.exceptions.InvalidNumberException;
 import it.univpm.WeatherCloseRomeApp.models.City;
 
+/**
+ * Interfaccia TempService che viene implementata da TempServiceImpl e contiene
+ * i metodi richiamati da TempController
+ * 
+ * @author Mattia Beccerica, Alessandro Fermanelli, Giulio Gattari
+ */
 public interface TempService {
-	
+
 	public org.json.simple.JSONObject APICall(int cnt) throws InvalidNumberException;
 
-	public org.json.simple.JSONArray getJSONList(int cnt) throws InvalidNumberException ;
+	public org.json.simple.JSONArray getJSONList(int cnt) throws InvalidNumberException;
 
-	public Vector<City> getVector(int cnt) throws InvalidNumberException ;
+	public Vector<City> getVector(int cnt) throws InvalidNumberException;
 
 	public org.json.simple.JSONObject save() throws IOException, ClassNotFoundException, InvalidNumberException;
 
